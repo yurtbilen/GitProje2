7315d8f42a5b642c35d415ece1a7208c3ed0e931
yurtbilen <t_yurtbilen@hotmail.com>
2023-01-25 21:51:35 +0300

    ikinci işi yaptım görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -2,7 +2,10 @@ package Gun01;
 
 public class Ogrenci {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merahba");
+
+        }
     }
 
 }
